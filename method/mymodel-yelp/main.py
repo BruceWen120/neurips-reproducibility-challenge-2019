b2cac7bfac272b3aa6c98cef0c424465ca315e9b
Zhi WEN <zwen8@mcb-gpu1.CS.McGill.CA>
2019-12-07 17:58:26 -0500

    training model

--- method/mymodel-yelp/main.py
+++ method/mymodel-yelp/main.py
@@ -19,7 +19,7 @@ from data import prepare_data, non_pair_data_loader, get_cuda, pad_batch_seuqenc
     to_var, calc_bleu, load_human_answer
 
 os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,3,4"
 
 ######################################################################################
 #  Environmental parameters
@@ -57,8 +57,8 @@ parser.add_argument('--label_size', type=int, default=1)
 args = parser.parse_args()
 
 args.if_load_from_checkpoint = False
-args.if_load_from_checkpoint = True
-args.checkpoint_name = "1557667911"
+# args.if_load_from_checkpoint = True
+# args.checkpoint_name = "1557667911"
 
 
 ######################################################################################
