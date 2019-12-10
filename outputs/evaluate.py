bab976030f3e1ca7942062c1dacc522efcc12823
amr-amr <andrei.romascanu@mail.mcgill.ca>
2019-12-10 18:07:15 -0500

    feat: srilm ppl

--- outputs/evaluate.py
+++ outputs/evaluate.py
@@ -1,45 +1,71 @@
 from typing import List
 from bleu import *
 import math
-from transformers import OpenAIGPTTokenizer, OpenAIGPTModel, OpenAIGPTLMHeadModel
-import torch
 import pandas as pd
 import fasttext
 import numpy as np
 from pathlib import Path
 from tqdm import tqdm
-
+from subprocess import PIPE, run
 
 # LM for perplexity
-model = OpenAIGPTLMHeadModel.from_pretrained('openai-gpt')
-model.eval()
-tokenizer = OpenAIGPTTokenizer.from_pretrained('openai-gpt')
+model = None
+tokenizer = None
 
-# classifier for acc
-fasttext_model = fasttext.load_model(f"../fasttext/{DATASET}_model.bin")
 
-def get_perplexity(sentence):
+def out(command):
+    result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
+    return result.stdout
+
+def get_perplexity(text: List[str], dataset: str):
+    tmp_corpus = Path("test_corpus_tmp.txt")
+    tmp_corpus.write_text("\n".join(text))
+    output = out(f"/usr/share/srilm/bin/i686-m64/ngram -lm  "
+                f"../srilm/{dataset}.corpus.lm -ppl  test_corpus_tmp.txt")
+
+    ppl = float(output.split("ppl=")[1].split("ppl1")[0])
+    ppl1 = float(output.split("ppl1=")[1])
+
+    return ppl
+
+def get_gpt2_perplexity(sentence):
+    global model
+    if model is None:
+        from transformers import OpenAIGPTTokenizer, OpenAIGPTModel, OpenAIGPTLMHeadModel
+        import torch
+        model = OpenAIGPTLMHeadModel.from_pretrained('openai-gpt')
+        model.eval()
+        tokenizer = OpenAIGPTTokenizer.from_pretrained('openai-gpt')
+
     tokenize_input = tokenizer.tokenize(sentence)
     tensor_input = torch.tensor([tokenizer.convert_tokens_to_ids(tokenize_input)])
-    loss=model(tensor_input, labels=tensor_input)
+    loss=model(tensor_input, lm_labels=tensor_input)
     return math.exp(loss[0].item())
 
 
-def evaluate(hyp: List[str], ref: List[str]):
+def evaluate(hyp: List[str], ref: List[str], dataset: str):
     print("Evaluating BLEU...")
     bleu, _ = corpus_bleu(hyp, [[x] for x in ref])
     bleu = bleu[0]
     print("\nBLEU: \n", bleu)
 
+    # print("Evaluating PPL (hyp)...")
+    # ppls = [get_gpt2_perplexity(s) for s in tqdm(hyp)]
+    # ppl = pd.DataFrame(ppls).describe()
+    # print("\nPPL: \n", ppl)
+    #
+    # print("Evaluating PPL (ref)...")
+    # ppls_ref = [get_gpt2_perplexity(s) for s in tqdm(ref)]
+    # ppl_ref = pd.DataFrame(ppls_ref).describe()
+    # print("\nPPL (ref): \n", ppl_ref)
+
     print("Evaluating PPL (hyp)...")
-    ppls = [get_perplexity(s) for s in tqdm(hyp)]
-    ppl = pd.DataFrame(ppls).describe()
+    ppl = get_perplexity(hyp, dataset)
     print("\nPPL: \n", ppl)
 
     print("Evaluating PPL (ref)...")
-    ppls_ref = [get_perplexity(s) for s in tqdm(ref)]
-    ppl_ref = pd.DataFrame(ppls_ref).describe()
-    print("\nPPL (ref): \n", ppl_ref)
+    ppl_ref = get_perplexity(ref, dataset)
+    print("\nPPL: \n", ppl_ref)
 
     print("Evaluating ACC...")
     labels = ["__label__pos" for _ in range(500)] + ["__label__neg" for _ in range(500)]
@@ -56,7 +82,6 @@ def evaluate(hyp: List[str], ref: List[str]):
     print("\nACC2 (pred on model = labels): \n", model_acc)
     print("\nACC3 (pred on model = pred on human): \n", model_same_as_human_acc)
 
-DATASET = "yelp"
 
 if __name__ == '__main__':
     hyp_dict = {
@@ -69,8 +94,13 @@ if __name__ == '__main__':
         "amazon": "human-amazon.txt",
         "imagecaption": "human-captions.txt"
     }
-    hyp = Path().read_text(hyp_dict[DATASET]).split("\n")
+
+    # classifier for acc
+    DATASET = "yelp"
+    fasttext_model = fasttext.load_model(f"../fasttext/{DATASET}_model.bin")
+
+    hyp = Path(hyp_dict[DATASET]).read_text().split("\n")
     hyp = [l for l in hyp if l != ""]
     ref = Path(ref_dict[DATASET]).read_text().split("\n")
     ref = [l for l in ref if l != ""]
-    evaluate(hyp, ref)
+    evaluate(hyp, ref, DATASET)
