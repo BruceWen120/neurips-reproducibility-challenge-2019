1291e3154440f8fe20a16bc646d4b054f8372b5c
Zhi WEN <zwen8@mcb-gpu1.CS.McGill.CA>
2019-12-11 19:36:16 -0500

    evaluate 128 latent size

--- method/mymodel-yelp/run_main.sh
+++ method/mymodel-yelp/run_main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=5,6,7,8 python main.py \
+CUDA_VISIBLE_DEVICES=6,7,8 python main.py \
     --transformer_model_size 512 \
     --latent_size 512 \
     --batch_size 1024
