d9777b1dea4cf8db66882f980b81f2eff0ad7adb
amr-amr <andrei.romascanu@mail.mcgill.ca>
2019-12-12 23:50:10 -0500

    feat: evaluate to work with result outputs and output evaluation as json

--- outputs/evaluate.py
+++ outputs/evaluate.py
@@ -7,6 +7,7 @@ import numpy as np
 from pathlib import Path
 from tqdm import tqdm
 from subprocess import PIPE, run
+import json
 
 # LM for perplexity
 model = None
@@ -44,9 +45,11 @@ def get_gpt2_perplexity(sentence):
 
 
 def evaluate(hyp: List[str], ref: List[str], dataset: str):
+    eval = {}
     print("Evaluating BLEU...")
     bleu, _ = corpus_bleu(hyp, [[x] for x in ref])
     bleu = bleu[0]
+    eval["bleu"] = bleu
     print("\nBLEU: \n", bleu)
 
     # print("Evaluating PPL (hyp)...")
@@ -61,15 +64,17 @@ def evaluate(hyp: List[str], ref: List[str], dataset: str):
 
     print("Evaluating PPL (hyp)...")
     ppl = get_perplexity(hyp, dataset)
+    eval["ppl"] = ppl
     print("\nPPL: \n", ppl)
 
     print("Evaluating PPL (ref)...")
     ppl_ref = get_perplexity(ref, dataset)
+    eval["ppl_ref"] = ppl_ref
     print("\nPPL: \n", ppl_ref)
 
     print("Evaluating ACC...")
     labels = ["__label__pos" for _ in range(500)] + ["__label__neg" for _ in range(500)]
-    pred_human = [fasttext_model.predict(l)[0][0] for l in ref]
+    pred_human  = [fasttext_model.predict(l)[0][0] for l in ref]
     pred_model = [fasttext_model.predict(l)[0][0] for l in hyp]
     human_correct = [1 if pred == true else 0 for pred,true in zip(pred_human, labels)]
     model_correct = [1 if pred == true else 0 for pred,true in zip(pred_model, labels)]
@@ -78,29 +83,82 @@ def evaluate(hyp: List[str], ref: List[str], dataset: str):
     model_acc = np.sum(model_correct) / len(model_correct)
     model_same_as_human_acc = np.sum(model_same_as_human) / len(model_same_as_human)
 
+    eval["human_acc"] = human_acc
+    eval["model_acc"] = human_acc
+    eval["model_same_as_human_acc"] = model_same_as_human_acc
     print("\nACC1 (pred on human = labels): \n", human_acc)
     print("\nACC2 (pred on model = labels): \n", model_acc)
     print("\nACC3 (pred on model = pred on human): \n", model_same_as_human_acc)
 
+    return eval
+
+
+def parse_results(f: Path):
+    """
+    Load text from results file f and parse.
+    File is generated with following format
+        gold: ever since joes has changed hands it 's gotten better and better .
+        2.0: ever since brother 's gotten worse it has always been just hands worse .
+        3.0: ever since brother has gotten worse it 's always been just worse and overpriced .
+        4.0: ever since brother has gotten worse it 's always been just hands worse .
+        5.0: ever since always has gotten worse it 's always been great and makes overpriced .
+        6.0: since always ever works has gotten great prices and it 's always worse .
+        7.0: since always has seasoned hands has gotten great prices and it 's always worse .
+        8.0: since always has seasoned hands has great prices and it 's always gotten worse .
+
+        gold: there is so much room in that part of the venue
+        ...
+
+    """
+    examples = [x.split("\n") for x in f.read_text()[1:-1].split("\n\n")]
+    model_out = [x[1:] for x in examples]
+
+    results_dict = {x[:3] : [] for x in model_out[0]}
+    for example in examples:
+        for x in example[1:]:
+            results_dict[x[:3]] += [x[5:]]
+
+    results_dict["gold"] = [x[0][6:] for x in examples]
+    return results_dict
+
 
 if __name__ == '__main__':
-    hyp_dict = {
-        "yelp": "my-model-yelp1.txt",
-        "amazon": "my-model-amazon.txt",
-        "imagecaption": "my-model-captions.txt"
-    }
-    ref_dict = {
-        "yelp": "human-yelp.txt",
-        "amazon": "human-amazon.txt",
-        "imagecaption": "human-captions.txt"
-    }
+    DATASET = "yelp"
+
+    original_results = False
+    if not original_results:
+        # load and parse results
+        results_path = Path("../results/yelp_1transformer_2019_12_12_11_09_39.txt")
+        results_dict = parse_results(results_path)
+        eval_path = results_path.parent / ("eval_" + results_path.name[:-4] + ".json")
+    else:
+        hyp_dict = {
+            "yelp": "my-model-yelp1.txt",
+            "amazon": "my-model-amazon.txt",
+            "imagecaption": "my-model-captions.txt"
+        }
+        ref_dict = {
+            "yelp": "human-yelp.txt",
+            "amazon": "human-amazon.txt",
+            "imagecaption": "human-captions.txt"
+        }
+        hyp = Path(hyp_dict[DATASET]).read_text().split("\n")
+        hyp = [l for l in hyp if l != ""]
+        ref = Path(ref_dict[DATASET]).read_text().split("\n")
+        ref = [l for l in ref if l != ""]
+        results_dict = {
+            "gold": ref,
+            "2.0" : hyp
+        }
+        eval_path = Path(hyp_dict[DATASET]).parent / ("eval_" + Path(hyp_dict[DATASET]).name + ".json")
 
     # classifier for acc
-    DATASET = "yelp"
     fasttext_model = fasttext.load_model(f"../fasttext/{DATASET}_model.bin")
 
-    hyp = Path(hyp_dict[DATASET]).read_text().split("\n")
-    hyp = [l for l in hyp if l != ""]
-    ref = Path(ref_dict[DATASET]).read_text().split("\n")
-    ref = [l for l in ref if l != ""]
-    evaluate(hyp, ref, DATASET)
+    # store eval results in dict
+    eval_dict = {}
+    ref = results_dict.pop("gold")
+    for w, hyp in results_dict.items():
+        eval_dict[w] = evaluate(hyp, ref, DATASET)
+
+    eval_path.write_text(json.dumps(eval_dict))
